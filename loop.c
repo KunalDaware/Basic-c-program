0abc98542a991a7b1b645b8fa0367e08a59407a3
Kunal Daware <dawarekunal26@gmail.com>
2022-06-21 19:48:46 +0530

    new file

---
+++ loop.c
@@ -0,0 +1,36 @@
+//Program to display 5 times Hello on screen useing  for loop
+
+
+
+# include<stdio.h>
+
+//demonstration of iteration
+
+void Display()    //defination
+{
+	int iCnt = 0;
+
+
+	//1 initialization
+	//2 displacement
+	//3 loop boday
+//         1             2          3
+for(iCnt=1; iCnt<=5; iCnt++)
+ {
+	printf("hello world\n");  //4	
+
+  }
+
+
+}
+
+int main()
+{
+	 Display();	    //function call
+
+     return 0;
+
+}
+
+
+
